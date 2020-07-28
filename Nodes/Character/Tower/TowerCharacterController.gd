0ec0533259b25ac4744c65019746cfbee2a757bc
Hiago de Souza <hiago7mg@gmail.com>
2020-07-28 16:39:00 -0300

    Create CombatStats node

--- Nodes/Character/Tower/TowerCharacterController.gd
+++ Nodes/Character/Tower/TowerCharacterController.gd
@@ -3,24 +3,24 @@ enum STATE{INACTIVE,DEFAULT, PICKUP}
 var state = STATE.INACTIVE
 var positioned_spot = null
 func _ready():
-	$PhysicCharacterBody/CharacterCollectionArea.controller = self
-	
+    $PhysicCharacterBody/CharacterCollectionArea.controller = self
+    
 func pickup_range_entered():
-	$PhysicCharacterBody/Selector.visible = true
+    $PhysicCharacterBody/Selector.visible = true
 
 func pickup_range_exited():
-	$PhysicCharacterBody/Selector.visible = false
+    $PhysicCharacterBody/Selector.visible = false
 
 func pickup():
-	if(positioned_spot):
-		positioned_spot.empty_spot()
-		positioned_spot = null
-	state = STATE.PICKUP
-	$PhysicCharacterBody/PhysicBodyShape.set_deferred("disabled",true)
+    if(positioned_spot):
+        positioned_spot.empty_spot()
+        positioned_spot = null
+    state = STATE.PICKUP
+    $PhysicCharacterBody/PhysicBodyShape.set_deferred("disabled",true)
 
 func drop(spot):
-	$PhysicCharacterBody/AnimationPlayer.play("Squish")
-	global_position = spot.global_position
-	spot.occupy_spot()
-	positioned_spot = spot
-	state = STATE.DEFAULT
+    $PhysicCharacterBody/AnimationPlayer.play("Squish")
+    global_position = spot.global_position
+    spot.occupy_spot()
+    positioned_spot = spot
+    state = STATE.DEFAULT
