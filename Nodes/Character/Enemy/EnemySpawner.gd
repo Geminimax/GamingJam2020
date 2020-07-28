262f38e018e11818824784bc0eb6709c6d5082fc
Hiago de Souza <hiago7mg@gmail.com>
2020-07-28 14:32:32 -0300

    Wave config list

--- Nodes/Character/Enemy/EnemySpawner.gd
+++ Nodes/Character/Enemy/EnemySpawner.gd
@@ -3,6 +3,7 @@ extends Node2D
 export (PackedScene) var GenericEnemy
 export (PackedScene) var RareEnemy
 
+export(Array,Resource) wave_configuration
 var nav_2d: Navigation2D = null setget setnav_2d
 var start = Vector2(70, 350)
 var end = Vector2(700, 450)
@@ -16,7 +17,7 @@ func _ready():
 	print("enemy spawner ready")
 
 func _process(delta):
-    pass
+	pass
 
 func gen_path(start: Vector2, end: Vector2):
 	var new_path = nav_2d.get_simple_path(start, end)
@@ -42,20 +43,20 @@ func spawn_wave():
 	$EnemyCooldown.start()
 
 func _on_EnemyCooldown_timeout():
-    enemies_count += 1
-    print("timeout enemy cooldown")
-    if enemies_count >= ENEMIES_PER_WAVE:
-        enemies_count = 0
-        wave = false
-        $EnemyCooldown.stop()
-    spawn_enemy()
+	enemies_count += 1
+	print("timeout enemy cooldown")
+	if enemies_count >= ENEMIES_PER_WAVE:
+		enemies_count = 0
+		wave = false
+		$EnemyCooldown.stop()
+	spawn_enemy()
 
 func spawn_enemy():
-    var enemy
-    if randf() <= 0.4:
-        enemy = RareEnemy.instance()
-    else:
-        enemy = GenericEnemy.instance()
-    add_child(enemy)
-    enemy.position = start
-    enemy.set_path(path)
+	var enemy
+	if randf() <= 0.4:
+		enemy = RareEnemy.instance()
+	else:
+		enemy = GenericEnemy.instance()
+	add_child(enemy)
+	enemy.position = start
+	enemy.set_path(path)
