ca198214a8894ab1707bab6d7f256ee05e167165
Hiago de Souza <hiago7mg@gmail.com>
2020-07-28 16:55:01 -0300

    Some targetting stuff

--- Nodes/Combat/CombatStats.gd
+++ Nodes/Combat/CombatStats.gd
@@ -7,12 +7,23 @@ export (int) var health
 export (int) var attack_range
 export (int) var attack_speed
 export (int) var attack_damage
+var valid_targets : Array = []
 
 func deal_damage(target : CombatStats):
     target.take_damage(attack_damage)
 
 func take_damage(damage_amount):
     health -= damage_amount
-    if(health <= 0):
+    if health <= 0:
         emit_signal("death")
     health = clamp(0,health,max_health)
+
+func add_to_valid_targets(target : CombatStats):
+    #If target isn't present:
+    if valid_targets.find(target) == -1:
+        valid_targets.append(target)
+
+func remove_from_valid_targets(target: CombatStats):
+    valid_targets.erase(target)
+    
+        
