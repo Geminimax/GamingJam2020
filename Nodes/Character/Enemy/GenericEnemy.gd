d12055958918e3d9e128856bfac45e35d3f84c2d
Alice Micheloni <alicemicheloni@tutanota.com>
2020-07-28 13:51:26 -0300

    enemy spawner half-done

--- Nodes/Character/Enemy/GenericEnemy.gd
+++ Nodes/Character/Enemy/GenericEnemy.gd
@@ -1,25 +1,20 @@
 extends Node2D
 
-onready var nav_2d: Navigation2D = $Navigation2D
+onready var nav_2d: Navigation2D = get_parent().get_node("Level")
 onready var caminho: Line2D = $Caminho
 
-var speed = 50.0
+var speed = 110.0
 var path = PoolVector2Array() setget set_path
 
 func _ready():
+    print("ENEMY READY")
     set_process(false)
-    var objective = get_parent().get_node("EnemyObjective").global_position
-    var new_path = nav_2d.get_simple_path(global_position, objective)
-    print(new_path)
-    caminho.points = new_path
-    set_path(new_path)
 
 func _process(delta):
     var move_dist = speed * delta
     move_along_path(move_dist)
 
 func move_along_path(distance: float):
-    print("move_along_path")
     var start_point = position
     for i in range(path.size()):
         var distance_to_next = start_point.distance_to(path[0])
@@ -29,9 +24,13 @@ func move_along_path(distance: float):
         elif path.size() == 1 and distance > distance_to_next:
             position = path[0]
             set_process(false)
+            handle_enemy_reaching_end()
         distance -= distance_to_next
         start_point = path[0]
         path.remove(0)
+
+func handle_enemy_reaching_end():
+    queue_free()
 
 func set_path(value: PoolVector2Array):
     path = value
