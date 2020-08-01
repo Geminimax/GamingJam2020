d06181f1f61d6ee34cb3856fb4d8fbd1dbd5c7ed
Hiago de Souza <hiago7mg@gmail.com>
2020-08-01 15:49:08 -0300

    Coin transparency change

--- Nodes/Currency.gd
+++ Nodes/Currency.gd
@@ -3,13 +3,14 @@ const FOLLOW_PLAYER_RADIUS = 80
 var approach_speed = 0
 var approach_accel = 50
 var velocity = Vector2()
-
+var initial_time = 1
 func _ready():
     set_as_toplevel(true)
+    initial_time = $Timer.wait_time
     
 func _process(delta):
     var player = Global.player
-    $AnimatedSprite.modulate.a = $Timer.time_left / 10
+    $AnimatedSprite.modulate.a = min(1.0,$Timer.time_left / (initial_time/2))
     if player:
         if global_position.distance_to(player.body.global_position) < FOLLOW_PLAYER_RADIUS:
             global_position = global_position + global_position.direction_to(player.body.global_position) * approach_speed * delta
