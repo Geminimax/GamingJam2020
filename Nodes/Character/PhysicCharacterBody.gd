03bac879847a10a96264acfd1fc7296a5b90abe0
Alice Micheloni <alicemicheloni@tutanota.com>
2020-08-01 12:05:28 -0300

    max level change

--- Nodes/Character/PhysicCharacterBody.gd
+++ Nodes/Character/PhysicCharacterBody.gd
@@ -4,5 +4,5 @@ class_name PhysicCharacterBody
 var velocity : Vector2 = Vector2()
 
 func _process(delta):
-	move_and_slide(velocity)
-	
+    move_and_slide(velocity)
+    
--- Nodes/UI/BaseHealthbar.gd
+++ Nodes/UI/BaseHealthbar.gd
@@ -3,12 +3,12 @@ extends Node2D
 var combat_stats : CombatStats setget set_combat_stats
 
 func set_combat_stats(value):
-	combat_stats = value
-	combat_stats.connect("health_changed",self,"on_health_changed")
-	on_health_changed()
+    combat_stats = value
+    combat_stats.connect("health_changed",self,"on_health_changed")
+    on_health_changed()
 
 func update_health_bar_value(current_value,max_value):
-	pass
+    pass
 
 func on_health_changed():
-	update_health_bar_value(combat_stats.health, combat_stats.max_health)
+    update_health_bar_value(combat_stats.health, combat_stats.max_health)
